12a0808a277fb702c7d8eb835eaa3cff52b34600
jonsamp <sampjon@gmail.com>
2021-01-15 13:00:06 -0500

    updates the app

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import JJImage from './team.jonathan.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>👋 Hello EAS</Text>
+      <Image
+        source={JJImage}
+        style={{
+          height: 200,
+          width: 200,
+          resizeMode: 'contain',
+          marginBottom: 32,
+        }}
+      />
+      <Text>👋 Hello EAS Update</Text>
       <StatusBar style='auto' />
     </View>
   );
